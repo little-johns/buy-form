f6598412cde16e10b23bb1757a6cc795d65b2238
Reza Lotfi <reza.lotfi14@gmail.com>
2019-04-16 17:20:36 -0700

    Change folder name to database

---
+++ database/index.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+const mongoUrl = 'mongodb://localhost/onehundredStocks';
+
+mongoose.connect(mongoUrl ,{ useNewUrlParser: true });
+const db = mongoose.connection;
+
+module.exports = db;

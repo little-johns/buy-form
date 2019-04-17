7b56f32b4713358ba55a38a1e6f9f10e274220d9
Reza Lotfi <reza.lotfi14@gmail.com>
2019-04-16 17:05:56 -0700

    Change from production mode to development mode

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var webpack = require('webpack');
 var path = require('path');
 
 module.exports  = {
-    mode: 'production',
+    mode: 'development',
     entry : __dirname + '/client/index.jsx',
     module : {
         rules : [

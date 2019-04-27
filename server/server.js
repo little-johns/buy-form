cbca01d2d81b545f363da9ec0f5d53ce2e0a2330
Reza Lotfi <reza.lotfi14@gmail.com>
2019-04-26 20:07:13 -0700

    Require newrelic and comment out bodyParser

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
+require('newrelic');
 var express = require('express');
-var bodyParser  = require('body-parser');
+// var bodyParser  = require('body-parser');
 var cors = require('cors');
 var db = require('../database/postgresQueries');
 
@@ -9,8 +10,8 @@ var app = express();
 
 //middleware
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+// app.use(bodyParser.urlencoded({ extended: false }))
+// app.use(bodyParser.json())
 
 app.use('/stocks/:id', express.static(__dirname + '/../public/'));
 

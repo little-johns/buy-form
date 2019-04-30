37b94ed199b1a752688f600911e006d2a28ebe65
Reza Lotfi <reza.lotfi14@gmail.com>
2019-04-30 15:17:31 -0700

    Implement redis query

--- database/postgresQueries.js
+++ database/postgresQueries.js
@@ -1,5 +1,11 @@
 const Pool = require('pg').Pool
 const config = require('./postgresConfig');
+const redis = require('redis');
+const client = redis.createClient();
+
+client.on('connect', () => {
+  console.log('redis client connected!!')
+})
 
 const pool = new Pool({
   user: 'postgres',
@@ -16,7 +22,9 @@ const getStock = (req, res) => {
       if (err) {
         throw err
       }
-      res.send(results.rows)
+      let row = results.rows
+      client.set(query, JSON.stringify(row))
+      res.send(row)
     })
   }
   else {
@@ -24,11 +32,25 @@ const getStock = (req, res) => {
       if (err) {
         throw err
       }
-      res.send(results.rows)
+      let row = results.rows
+      client.set(query, JSON.stringify(row))
+      res.send(row)
     })
   }
 }
 
+const getCache = (req, res) => {
+  let query = req.params.query;
+  client.get(query, (err, result) => {
+    if (result) {
+      res.send(result);
+    } else {
+      getStock(req, res);
+    }
+  })
+}
+
 module.exports = {
   getStock,
+  getCache
 }
